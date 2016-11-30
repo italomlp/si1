a14f32decff6b61538427e2c452c72a4fe095d74
Italo Menezes <italo.poroca@ccc.ufcg.edu.br>
2016-11-30 16:10:28 -0300

    Barra de progresso funcionando.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
 var app = angular.module('todoList', []);
 app.controller('TasksController', function() {
-	this.tasksArray = ["Cortar a grama", "Ir ao supermercado", "Fazer o jantar"];
+	this.actualPercent = 0;
+	this.tasksArray = [
+						{name : "Cortar a grama", done : false}, 
+						{name : "Ir ao supermercado", done : false}, 
+						{name : "Fazer o jantar", done : false}
+					];
 	
 	this.add = function (taskName) {
-		this.tasksArray.push(taskName);
+		this.tasksArray.push({name : taskName, done : false});
+		this.calculatePercent();
 	};
 	
 	this.clear = function () {
 		this.tasksArray = [];
+		this.actualPercent = 0;
+	};
+	
+	this.calculatePercent = function () {
+		var tasksDone = 0;
+		for (var i = 0; i < this.tasksArray.length; i++) {
+			if (this.tasksArray[i].done == true) {
+				tasksDone = tasksDone + 1;
+			}
+		}
+		if (tasksDone != 0) {
+			this.actualPercent = (100 * tasksDone) / this.tasksArray.length;
+		}
 	};
 });
